3baa928055f7c1d7f5c05a37e8d66f4332847a98
rustiphyde <rustiphyde@gmail.com>
2019-12-12 00:42:53 -0600

    Add Thymeleaf template that returns a dynamic list

--- src/main/java/org/launchcode/hellospring/controllers/HelloController.java
+++ src/main/java/org/launchcode/hellospring/controllers/HelloController.java
@@ -1,7 +1,11 @@
 package org.launchcode.hellospring.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Controller
 public class HelloController {
@@ -24,22 +28,36 @@ public class HelloController {
     // Handler that handles requests of the form /hello?name=Rustiphyde
 
     @RequestMapping(method = {RequestMethod.GET, RequestMethod.POST}, value = "hello")
-    @ResponseBody
-    public String helloWithQueryParam(@RequestParam String name) {
-        return "Hello, " + name + "!";
+    public String helloWithQueryParam(@RequestParam String name, Model model) {
+        String aGreeting = "Hello, " + name + "!";
+        model.addAttribute("greeting", aGreeting);
+        return "hello";
     }
 
     // Handles requests of the form /hello/Rusty
     @GetMapping("hello/{name}")
-    @ResponseBody
-    public String helloWithPathParam(@PathVariable String name){
-        return "Hello, " + name + "!";
+    public String helloWithPathParam(@PathVariable String name, Model model){
+        model.addAttribute("greeting", "Hello, " + name + "!");
+        return "hello";
     }
 
     // Displays the form
     @GetMapping("form")
     public String helloForm(){
         return "form";
+    }
+
+
+
+    @GetMapping("hello-names")
+    public String helloNames(Model model){
+        List<String> names = new ArrayList<>();
+        names.add("Rusty");
+        names.add("Rustiphyde");
+        names.add("Rustipher");
+        names.add("Rustoleum Dynamite");
+        model.addAttribute("names", names);
+        return "hello-list";
     }
 
 }
